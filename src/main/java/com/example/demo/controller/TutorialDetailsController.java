e75aaac98005db1596ccf6b472f13b7136dd685a
ericdev0212 <eric.devos0212@gmail.com>
2023-03-07 01:01:27 +0900

    first commit

---
+++ src/main/java/com/example/demo/controller/TutorialDetailsController.java
@@ -0,0 +1,67 @@
+package com.example.demo.controller;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.Tutorial;
+import com.example.demo.model.TutorialDetails;
+import com.example.demo.repository.TutorialDetailsRepository;
+import com.example.demo.repository.TutorialRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api")
+public class TutorialDetailsController {
+
+    @Autowired
+    TutorialRepository tutorialRepository;
+    @Autowired
+    TutorialDetailsRepository detailsRepository;
+    @PostMapping("/tutorials/{tutorialId}/details")
+    public ResponseEntity<TutorialDetails> createDetails(@PathVariable(value = "tutorialId") Long tutorialId, @RequestBody TutorialDetails detailsRequest) {
+        Tutorial tutorial = tutorialRepository.findById(tutorialId)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found Tutorial with id = " + tutorialId));
+
+        detailsRequest.setCreatedOn(new java.util.Date());
+        detailsRequest.setTutorial(tutorial);
+
+        TutorialDetails details = detailsRepository.save(detailsRequest);
+        return new ResponseEntity<>(details, HttpStatus.CREATED);
+    }
+
+    @GetMapping({"/details/{id}", "/tutorials/{id}/details"})
+    public ResponseEntity<TutorialDetails> getDetailsById(@PathVariable("id") Long id) {
+        TutorialDetails details = detailsRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found Tutorial Details with id = " + id));
+        return new ResponseEntity<>(details, HttpStatus.OK);
+
+    }
+
+    @PutMapping("/details/{id}")
+    public ResponseEntity<TutorialDetails> updateDetails(@PathVariable("id") Long id, @RequestBody TutorialDetails detailsRequest) {
+        TutorialDetails details = detailsRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("id " + id + " not found"));
+        details.setCreatedBy(detailsRequest.getCreatedBy());
+        return new ResponseEntity<>(detailsRepository.save(details), HttpStatus.OK);
+    }
+    @DeleteMapping("/details/{id}")
+    public ResponseEntity<HttpStatus> deleteDetails(@PathVariable("id") long id) {
+        detailsRepository.deleteById(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/tutorials/{tutorialId}/details")
+    public ResponseEntity<TutorialDetails> deleteDetailsOfTutorial(@PathVariable(value = "tutorialId") Long tutorialId) {
+        if (!tutorialRepository.existsById(tutorialId)) {
+            throw new ResourceNotFoundException("Not found Tutorial with id = " + tutorialId);
+        }
+
+        detailsRepository.deleteByTutorialId(tutorialId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+}
